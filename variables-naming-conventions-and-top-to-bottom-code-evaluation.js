a0913373bbcae773bb2a5f4ad14ae40b79db62d9
gowthami <ganja.gowthami@gmail.com>
2025-09-09 23:36:00 -0500

    enhance

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,9 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let name = "Alice";
+let items = 5;
+let cost = 20;
+let sentence = name + " bought " + items + " items for $" + cost + ".";
 
-console.log(d);
+console.log(sentence);
